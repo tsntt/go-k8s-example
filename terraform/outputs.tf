c4c05fc673b0e5c0b5a31d31cf84560e9b097659
tsntt <tsn.teo@proton.me>
2025-09-24 15:34:22 -0300

    remove terraform and added db persistence

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "The ID of the EC2 instance."
-  value       = aws_instance.go_api_instance.id
-}
-
-output "security_group_id" {
-  description = "The ID of the Security Group."
-  value       = aws_security_group.app_sg.id
-}
